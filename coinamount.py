eac66f48f70470e9d5f364a2ff2bd8378156e646
Kendell-B <103529228+Kendell-B@users.noreply.github.com>
2022-09-25 18:39:58 -0500

    Update coinamount.py

--- coinamount.py
+++ coinamount.py
@@ -1,3 +1,5 @@
+import random
+
 charge_amount = random.randint(1, 10000)
 amount_paid = random.randint(10000, 20000)
 amount_2_count = amount_paid - charge_amount
@@ -16,3 +18,14 @@ print(c//2000, "twenties")
 c = c%2000
 print(c//1000, "tens")
 c = c%1000
+print(c//500, "fives")
+c = c%500
+print(c//100, "dollars")
+c = c%100
+print(c//25, "quarters")
+c = c%25
+print(c//10, "dimes")
+c = c%10
+print(c//5, "nickles")
+c = c%5
+print(c//1, "pennies")
